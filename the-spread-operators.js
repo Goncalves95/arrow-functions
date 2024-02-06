2258529f3a4ef11689710fed9d09849c1f9e4fbd
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2024-02-06 13:22:50 +0100

    Nerw arr1 and arr2

---
+++ the-spread-operators.js
@@ -0,0 +1,14 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node the-spread-operator.js in the terminal
+ */
+
+// No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+console.log("Second array:", arr2);
+// Copying an array
+
+// Copying an object
+
+// Copying only part of an array/object
